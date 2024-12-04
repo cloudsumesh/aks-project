3da591d5b51dbbbc02acc062efb61da4d105c36a
cloudsumesh <sumeshchandran.github@gmail.com>
2024-12-04 10:27:17 +0200

    Azure Login Demo

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-resource "local_file" "demo" {
-  filename = "sample.txt"
-  content = "This is for demo"
-  
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = "Demo"
--- provider.tf
+++ provider.tf
@@ -1,9 +1,6 @@
 terraform {
   required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = "2.5.2"
-    }
+
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.9.0"
@@ -11,9 +8,7 @@ terraform {
   }
 }
 
-provider "local" {
-  # Configuration options
-}
+
 provider "azurerm" {
   features {}
 
